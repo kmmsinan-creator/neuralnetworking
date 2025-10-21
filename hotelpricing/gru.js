d1b97534b80ebaf0b7101758d056efe0b294f410
Mohd Sinan <kmmsinan@gmail.com>
2025-10-21 22:12:31 +0300

    Update gru.js

--- hotelpricing/gru.js
+++ hotelpricing/gru.js
@@ -4,25 +4,9 @@ function visualizeGRU(data) {
   const dates = data.map(d => d.date);
 
   Plotly.newPlot("gruChart", [
-    {
-      x: dates,
-      y: actual,
-      mode: "lines",
-      name: "Actual ADR",
-      line: { color: "#1f77b4" },
-    },
-    {
-      x: dates,
-      y: predicted,
-      mode: "lines",
-      name: "Predicted ADR (GRU)",
-      line: { color: "#ff7f0e" },
-    },
-  ], {
-    title: "GRU Model Forecast: Actual vs Predicted ADR",
-    xaxis: { title: "Date" },
-    yaxis: { title: "Average Daily Rate" },
-  });
+    { x: dates, y: actual, mode: "lines", name: "Actual ADR", line: { color: "#1f77b4" } },
+    { x: dates, y: predicted, mode: "lines", name: "Predicted ADR (GRU)", line: { color: "#ff7f0e" } }
+  ], { title: "GRU Model Forecast: Actual vs Predicted ADR", xaxis: { title: "Date" }, yaxis: { title: "Average Daily Rate" } });
 
   document.getElementById("modelInfo").innerHTML = `
     <p><b>Model Used:</b> GRU (Gated Recurrent Unit)</p>
